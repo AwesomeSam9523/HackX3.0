25b3ab9abb00d93e38dfb5c94acd2ff0ddc5cb04
thecoderwithHat <vaibhavtheblogger@gmail.com>
2025-06-22 21:19:24 +0530

    cosmetic fixes

--- hackxfrontend/src/app/page.tsx
+++ hackxfrontend/src/app/page.tsx
@@ -5,10 +5,22 @@ import Navbar from "@/components/navbar";
 
 export default function Home() {
   return (
-    <main className="bg-black">
+    <main className="bg-black relative overflow-x-clip">
       <Navbar/>
-      <Timer />
-      <Themes />
+      <div
+        className="ellipseDiv absolute left-[72%] top-0 -translate-x-1/2 z-0"
+        style={{
+          width: '100%',
+          height: '864px',
+          filter: 'blur(211.1px)',
+          borderRadius: '50%',
+          background: 'radial-gradient(50% 50% at 50% 50%, #1a252f, #111)',
+        }}
+      />
+    
+        <Timer />
+        <Themes />
+      
       <Sponsors/>
     </main>
   );
