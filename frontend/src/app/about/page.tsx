464b3d7442fae2f436c280e7388c6634c0531ec5
Kanishka Bhatia <bhatiakanishka930@gmail.com>
2025-06-29 16:44:46 +0530

    refactor : added white corner borders

--- frontend/src/app/about/page.tsx
+++ frontend/src/app/about/page.tsx
@@ -2,11 +2,13 @@ import PastSponsors from "@/components/past_sponsers";
 import CommunityPartners from "@/components/CommunityPartners";
 import WhyUsSection from "@/components/WhyUsSection";
 import Background from "@/components/Background";
+import Navbar from "@/components/Navbar";
 
 export default function About() {
   return (
     <main>
       <Background/>
+      <Navbar/>
 <WhyUsSection/>
       <PastSponsors />
       <CommunityPartners />
--- frontend/src/components/WhyUsSection.tsx
+++ frontend/src/components/WhyUsSection.tsx
@@ -13,7 +13,13 @@ const WhyUsSection = () => {
         </div>
       </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2 justify-items-center py-10 px-85 ">
+      <div className="relative grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 justify-items-center py-6 px-4 max-w-[1100px] mx-auto">
+  {/* Corner Borders */}
+  <span className="absolute -top-2 -left-2 h-[15px] w-[15px] border-3 border-white border-b-0 border-r-0" />
+  <span className="absolute -top-2 -right-2 h-[15px] w-[15px] border-3 border-white border-b-0 border-l-0" />
+  <span className="absolute -bottom-2 -left-2 h-[15px] w-[15px] border-3 border-white border-t-0 border-r-0" />
+  <span className="absolute -bottom-2 -right-2 h-[15px] w-[15px] border-3 border-white border-t-0 border-l-0" />
+
   {cardData.map((card: CardProps, index: number) => (
     <Card
       key={index}
@@ -23,6 +29,7 @@ const WhyUsSection = () => {
     />
   ))}
 </div>
+
 
     </div>
   );
