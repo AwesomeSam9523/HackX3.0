36b0e279613e18601b5d6efc1d89339fa896886b
AwesomeSam9523 <samakshgupta04@gmail.com>
2025-07-01 19:55:53 +0530

    Remove useless comments

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,24 +3,23 @@ import Themes from "@/components/Themes";
 import Sponsors from "@/components/Sponsors";
 import HeroSection from "@/components/Hero";
 import Background from "@/components/Background";
-import Spline from '@splinetool/react-spline/next';
-
+import Spline from "@splinetool/react-spline/next";
 
 export default function Home() {
   return (
     <main className="relative overflow-x-clip">
-      <Background/>
+      <Background />
       <HeroSection />
       <div className="h-12" />
       {/* Replaced existing ellipseDiv with Spline component */}
       <div
         className="absolute inset-0 z-5"
         style={{
-          pointerEvents: "none", // Ensures the Spline canvas doesn't capture mouse events
-          width: "90%", // Made smaller
-          height: "90vh", // Made smaller
-          top: '-63vh', // Adjusted top to keep it visible while smaller
-          left: '5%', // Center horizontally if width is 80%
+          pointerEvents: "none",
+          width: "90%",
+          height: "90vh",
+          top: "-63vh",
+          left: "5%",
         }}
       >
         <Spline scene="https://prod.spline.design/WWQ6UfiQ4jUgZ9zx/scene.splinecode" />
