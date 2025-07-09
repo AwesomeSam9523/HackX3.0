7358c2ec504256cd2f2f42b43b5161a98f963688
AwesomeSam9523 <samakshgupta04@gmail.com>
2025-07-10 01:29:03 +0530

    lint backend

--- backend/src/middlewares/error.ts
+++ backend/src/middlewares/error.ts
@@ -1,48 +1,31 @@
+import {NextFunction, Request, Response} from "express";
+import ErrorHandler from "../utils/errorHandler.js";
+import {envMode} from "../app.js";
 
-  import { NextFunction, Request, Response } from "express";
-  import ErrorHandler from "../utils/errorHandler.js";
-  import { envMode } from "../app.js";
-  
-  export const errorMiddleware = (
-   err:ErrorHandler,
-   req:Request,
-   res:Response,
-   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-   next:NextFunction
-  )=> {
-  
-   err.message||= "Internal Server Error";
-   err.statusCode = err.statusCode || 500;
-    
-   const response:{
+export const errorMiddleware = (
+  err: ErrorHandler,
+  req: Request,
+  res: Response,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  next: NextFunction
+) => {
+
+  err.message ||= "Internal Server Error";
+  err.statusCode = err.statusCode || 500;
+
+  const response: {
     success: boolean,
     message: string,
-    error?:ErrorHandler
-   } = {
-      success: false,
-      message: err.message,
-   };
-  
-   if (envMode === "DEVELOPMENT") {
-     response.error = err;
-   }
-  
-   return res.status(err.statusCode).json(response);
-  
+    error?: ErrorHandler
+  } = {
+    success: false,
+    message: err.message,
   };
-  
-  type ControllerType = (
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ) => Promise<void | Response<unknown, Record<string, unknown>>>;
-  
-  export const TryCatch = (passedFunc:ControllerType) => async (req:Request, res:Response, next:NextFunction) => {
-   try {
-     await passedFunc(req, res, next);
-   } catch (error) {
-      next(error);
-    }
-  };
-  
-  
+
+  if (envMode === "DEVELOPMENT") {
+    response.error = err;
+  }
+
+  return res.status(err.statusCode).json(response);
+};
+
