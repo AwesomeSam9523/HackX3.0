1118899462acf7435d9fb5db48dec7ee8e722e0f
AwesomeSam9523 <samakshgupta04@gmail.com>
2025-07-12 08:33:19 +0530

    Make background and XComponent SSR

--- frontend/src/components/XComponent.tsx
+++ frontend/src/components/XComponent.tsx
@@ -1,19 +1,14 @@
-"use client";
-
 import Image from "next/image";
-import { useIsMobile } from "@/hooks/isMobile";
 
 const XComponent = () => {
-  const isMobile = useIsMobile();
-  const src = isMobile ? "/x2Mobile.png" : "/x2.png";
   return (
     <div className="pointer-events-none absolute top-0 left-0 z-6 flex h-screen w-full items-start mix-blend-overlay lg:items-center lg:justify-center">
       <Image
-        src={src}
+        src={"/x2.svg"}
         alt="Background decoration"
         width={384}
         height={384}
-        className="h-auto w-full blur-[0px] lg:h-full lg:w-auto lg:blur-none"
+        className="absolute left-1/2 h-auto w-[40rem] max-w-none -translate-x-1/2 transform sm:static sm:left-0 sm:h-full sm:w-auto sm:translate-x-0 sm:transform-none lg:max-w-full"
         priority
       />
     </div>
